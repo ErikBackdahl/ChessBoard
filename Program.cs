7ac406ca6dd4b31c69f7f198763882a867690279
ErikBackdahl <erik.backdahl@gmail.com>
2024-09-29 21:18:29 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,73 @@
-﻿namespace ChessBoard
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ChessBoard
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            int size;
+
+            //gets int input from user
+
+            while (true)
+            {
+                Console.WriteLine("Write in a number to decide how large the chessboard will be:");
+
+                dynamic number = Console.ReadLine();
+
+                bool success = (int.TryParse(number, out size));
+                if(success)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("failed must input must be an integer");
+                }
+            }
+
+            //creates the chessboard
+            
+            for (int i = 0; i <size; i++)
+            {
+                Console.WriteLine();
+               
+                if (i % 2 == 0)
+                {
+                    for (int ii = 0; ii < size; ii++)
+                    {
+                        if (ii % 2 == 0)
+                        {
+                            Console.Write("◼︎ ");
+
+                        }
+                        else
+                        {
+                            Console.Write("◻︎ ");
+                        }
+                    }
+                }
+                else
+                {
+                    for (int iii = 0; iii < size; iii++)
+                    {
+                        if (iii % 2 != 0)
+                        {
+                            Console.Write("◼︎ ");
+
+                        }
+                        else
+                        {
+                            Console.Write("◻︎ ");
+                        }
+
+
+                    }
+
+                }
+            }
         }
     }
 }
